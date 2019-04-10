99877424b1dd4c3b2820b99686176ea5a7dc6ace
Mike Crabtree <Mdcrab02@users.noreply.github.com>
2019-04-10 18:58:51 -0400

    Update requirements.sh
    Updated package list requirements

--- requirements.sh
+++ requirements.sh
@@ -4,12 +4,12 @@ set -e
 # Install specific package versions with pip
 # Check the latest version using https://pypi.org/search
 
-# pip3 install pandas==0.23.3
-# pip3 install matplotlib==2.2.2
-# pip3 install scikit-learn==0.19.2
-# pip3 install numpy==1.15.0
+# pip3 install pandas==0.24.1
+# pip3 install matplotlib==3.0.2
+# pip3 install scikit-learn==0.20.2
+# pip3 install numpy==1.15.4
 # pip3 install seaborn==0.9.0
-# pip3 install bokeh==0.13.0
+# pip3 install xgboost==0.8.0
 
 
 # Include datasets in the image
